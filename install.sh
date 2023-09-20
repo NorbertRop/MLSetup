32f554e35574e541888ac74e584fe764748add2b
Norbert Ropiak <norbert.rop@gmail.com>
2023-09-20 10:40:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -135,10 +135,6 @@ elif [ "$OS" == "Linux" ]; then
     export PYENV_ROOT="$HOME/.pyenv"
     command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-    echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-
     sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev curl \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
@@ -149,6 +145,7 @@ elif [ "$OS" == "Linux" ]; then
 
     # install poetry
     curl -sSL https://install.python-poetry.org | python3 -
+    echo 'export PATH="~/.local/bin:$PATH"' >> ~/.zshrc
 else
     exit 1
 fi
