60f4ea5c48df51f4e0b5bf9be7c4c86c8c491956
Norbert Ropiak <norbert.rop@gmail.com>
2024-12-19 12:05:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -129,6 +129,9 @@ elif [ "$OS" == "Linux" ]; then
     sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
     sed -i "s/plugins=(git)/${ZSHRC_PLUGINS}/" ~/.zshrc
 
+    echo 'source "$HOME/.rye/env"' >> ~/.zshrc
+    source ~/.zshrc
+
     curl -sSf https://rye.astral.sh/get | bash
     mkdir $ZSH_CUSTOM/plugins/rye
     rye self completion -s zsh > $ZSH_CUSTOM/plugins/rye/_rye
